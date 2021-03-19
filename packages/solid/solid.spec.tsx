7b54e8a68ec7f30b5c18086698bedaca7efb0587
Alexandre <amoutonbrady@gmail.com>
2021-03-20 00:53:30 +0100

    test: Adding test for renderToString and renderToStringAsync

--- packages/solid/solid.spec.tsx
+++ packages/solid/solid.spec.tsx
@@ -1,20 +1,16 @@
+// @jsxImportSource solid-js
 import { suite } from 'uvu'
 import * as assert from 'uvu/assert'
 
 import { TW } from 'twind'
 import type { VirtualSheet } from 'twind/sheets'
 
-import { create } from 'twind'
+import { create, tw } from 'twind'
 import { virtualSheet } from 'twind/sheets'
-import { renderToString } from 'solid-js/web'
+import { createResource, Suspense } from 'solid-js'
+import { renderToString as solidRenderToString } from 'solid-js/web'
 
-import { styled } from '.'
-
-// React is messing with JSX types
-// Disabling them  for now..
-declare module 'react' {
-  namespace JSX {}
-}
+import { styled, renderToString, renderToStringAsync } from './index'
 
 const test = suite<{ sheet: VirtualSheet; tw: TW }>('@twind/solid')
 
@@ -45,7 +41,7 @@ test('styled', ({ tw, sheet }) => {
 
   assert.equal(sheet.target, [])
 
-  const { html } = renderToString(() => (
+  const { html } = solidRenderToString(() => (
     <Button size="lg" class="x" type="button">
       Click Me
     </Button>
@@ -53,6 +49,41 @@ test('styled', ({ tw, sheet }) => {
 
   assert.is(html, '<button type="button" class="tw-xz6gsm tw-jo04kc x">Click Me</button>')
   assert.equal(sheet.target, ['.tw-xz6gsm{font-size:1.125rem;line-height:1.75rem}'])
+})
+
+test('renderToString emits critical CSS', () => {
+  const { html, styles } = renderToString(
+    () => <button class={tw`bg-blue-500 text-gray-50`}>Click Me</button>,
+    { preflight: false },
+  )
+
+  assert.ok(html.includes('bg-blue-500 text-gray-50'))
+  assert.ok(styles.includes('bg-blue-500'))
+  assert.ok(styles.includes('text-gray-50'))
+})
+
+test.skip('renderToStringAsync emits critical CSS', async () => {
+  const { html, styles } = await renderToStringAsync(
+    () => {
+      const [text] = createResource(
+        'weDontCareAboutThis',
+        (): Promise<string> => {
+          return new Promise((res) => setTimeout(res, 1000, 'Click Me'))
+        },
+      )
+
+      return (
+        <Suspense fallback="Loading...">
+          <button class={tw`bg-blue-500 text-gray-50`}>{text()}</button>
+        </Suspense>
+      )
+    },
+    { preflight: false },
+  )
+
+  assert.ok(html.includes('bg-blue-500 text-gray-50'))
+  assert.ok(styles.includes('bg-blue-500'))
+  assert.ok(styles.includes('text-gray-50'))
 })
 
 test.run()
