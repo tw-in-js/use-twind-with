b5948d235e4db98b847ebb908a4bf2f8e5f269c9
gojutin <gojutin@gmail.com>
2021-06-21 10:47:45 -0400

    fix(next): linting issues

--- packages/next/shim/document.ts
+++ packages/next/shim/document.ts
@@ -12,17 +12,17 @@ export type Constructor<T = object, S = object> = (new (...input: any[]) => T) &
 
 export default function withTwindDocument<
   P = {},
-  Base extends Constructor<Document<P>, typeof Document> = typeof Document
+  Base extends Constructor<Document<P>, typeof Document> = typeof Document,
 >(config?: Configuration, BaseDocument?: Base): Base
 
 export default function withTwindDocument<
   P = {},
-  Base extends Constructor<Document<P>, typeof Document> = typeof Document
+  Base extends Constructor<Document<P>, typeof Document> = typeof Document,
 >(BaseDocument?: Base): Base
 
 export default function withTwindDocument<
   P = {},
-  Base extends Constructor<Document<P>, typeof Document> = typeof Document
+  Base extends Constructor<Document<P>, typeof Document> = typeof Document,
 >(configOrBase?: Configuration | Base, BaseDocument?: Base): Base {
   // @ts-ignore
   return class extends withTwind(configOrBase as Configuration, BaseDocument) {
